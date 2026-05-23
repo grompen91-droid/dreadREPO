fe69e271ca297260b445c65321fecaa9b338a7a6
Per <123196452+noxaur@users.noreply.github.com>
2026-05-23 02:29:57 +0200

    fix: move UnityWebRequestAsyncOperation to UnityWebRequestModule stub to fix MissingMethodException (#156)
    * fix: move UnityWebRequestAsyncOperation to UnityWebRequestModule stub to fix MissingMethodException
    
    * fix: add UnityWebRequestModule stub verifier check for hardening

---
+++ .github/scripts/UnityEngine.UnityWebRequestModule_stubs.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+
+namespace UnityEngine.Networking
+{
+    public class UnityWebRequest : IDisposable
+    {
+        public UnityWebRequest() { }
+        public UnityWebRequest(string url, string method) { }
+        public UploadHandlerRaw uploadHandler { get; set; }
+        public DownloadHandlerBuffer downloadHandler { get; set; }
+        public string method { get; set; }
+        public Result result { get; }
+        public string error { get; }
+        public UnityWebRequestAsyncOperation SendWebRequest() => null;
+        public void SetRequestHeader(string name, string value) { }
+        public void Dispose() { }
+        public enum Result { Success }
+    }
+    public class UploadHandlerRaw
+    {
+        public UploadHandlerRaw(byte[] data) { }
+    }
+    public class DownloadHandlerBuffer
+    {
+        public string text { get; }
+    }
+    public class UnityWebRequestMultimedia
+    {
+        public static UnityWebRequest GetAudioClip(string url, AudioType audioType) => null;
+    }
+    public class DownloadHandlerAudioClip
+    {
+        public static AudioClip GetContent(UnityWebRequest req) => null;
+    }
+    public class UnityWebRequestAsyncOperation : AsyncOperation { }
+}
